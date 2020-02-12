dd9ace35fd0ed995556d85547231e3bbd07723bb
msdn user03 <msdn.user03@sistemasesco.com>
2020-02-12 16:24:27 -0300

    Se agrega caso test_parametria como ejemplo para conexion a la base

--- pruebas_parametria/conftest.py
+++ pruebas_parametria/conftest.py
@@ -1,4 +1,6 @@
 import pytest
+import pyodbc
+
 
 @pytest.fixture
 def supply_parametria():
@@ -6,3 +8,17 @@ def supply_parametria():
 	bb= 20
 	cc= 30
 	return [aa,bb,cc]
+
+@pytest.fixture
+def supply_sql():
+	conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=V21SERVER21\V21SERVER21_CSAS;'
+                      'Database=vfQAUI_07_00_12;'
+                      'Trusted_Connection=yes;'
+					  'UID=QAUserAutomation;'
+					  'PWD=vesco;')
+
+	cursor = conn.cursor()
+	cursor.execute('SELECT TOP 1 CodInterfaz, Nombre FROM FONDOS')
+	
+	return cursor
--- pruebas_parametria/test_parametria.py
+++ pruebas_parametria/test_parametria.py
@@ -1,5 +1,16 @@
 import pytest
+import logging
 
 def test_parametria_A(supply_parametria):
     texto = supply_parametria
+    print("INFO: ")
     print(texto)
+
+def test_parametria_B(supply_sql):
+    cursor = supply_sql.fetchone()
+    print("\n LISTA: ")
+    for row in cursor:
+        print(" "+row)
+
+    print("NumFondo: "+cursor[0])    
+    print("Nombre: "+cursor[1]) 
